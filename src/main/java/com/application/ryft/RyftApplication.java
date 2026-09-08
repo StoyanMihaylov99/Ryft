089bf25295a5a17813f949486653ccb07e46f3e8
StoyanMihaylov99 <stoqn.business@gmail.com>
2026-09-08 15:29:39 +0200

    Initial commit of Ryft Spring Boot project

---
+++ src/main/java/com/application/ryft/RyftApplication.java
@@ -0,0 +1,13 @@
+package com.application.ryft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/ryft/RyftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.ryft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
